0ea41be05e4dc297aeb49305f7002154873b046d
Mikko Malmari <mikko.malmari@gmail.com>
2018-01-28 00:26:22 +0200

    Testing autodeploy script

--- resources/views/tuomas.blade.php
+++ resources/views/tuomas.blade.php
@@ -64,6 +64,7 @@
                                                 </table>
                                                 <div class="image main"><img src="images/xwing.png" alt="pew pew"/></div>
                                         </section>
+                                        <!-- -->
                                     </div>
 		<!-- Scripts -->
                 <script src="{{asset('/js/jquery.min.js')}}"></script>
