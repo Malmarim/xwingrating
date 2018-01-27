1734f2b649ab23ded760bdf4759bb3bab2d29064
Ubuntu <mikkom@mikko-dev.334a5r1nmpzuxf0jkflbem3bnc.fx.internal.cloudapp.net>
2018-01-28 00:12:53 +0200

    Added git-hook config

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+	Webklex\GitHook\Providers\LaravelServiceProvider::class,
 
         /*
          * Package Service Providers...
